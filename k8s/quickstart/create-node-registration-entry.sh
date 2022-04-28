093399d17d3d55ff4af6a3ce1b6c89da93c0b05d
Priya Wadhwa <priya@chainguard.dev>
2022-04-28 15:49:20 -0500

    Set up config for using oidc.priya-chainguard.dev

--- k8s/quickstart/create-node-registration-entry.sh
+++ k8s/quickstart/create-node-registration-entry.sh
@@ -10,7 +10,7 @@ echo "${bb}Creating registration entry for the node...${nn}"
 kubectl exec -n spire spire-server-0 -- \
     /opt/spire/bin/spire-server entry create \
     -node  \
-    -spiffeID spiffe://example.org/ns/spire/sa/spire-agent \
+    -spiffeID spiffe://oidc.priya-chainguard.dev/ns/spire/sa/spire-agent \
     -selector k8s_sat:cluster:demo-cluster \
     -selector k8s_sat:agent_ns:spire \
     -selector k8s_sat:agent_sa:spire-agent
